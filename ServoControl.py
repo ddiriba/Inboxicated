8c05f027c983488c6a6ba380e45ca9c69762db32
JohnBaumb <80135794+JohnBaumb@users.noreply.github.com>
2021-12-07 13:06:39 -0800

    Function Call must now include "Speed" Variable 1-10

--- ServoControl.py
+++ ServoControl.py
@@ -7,21 +7,29 @@ from time import sleep
 servo = AngularServo(17, min_angle=0, max_angle=270, min_pulse_width=0.0005, max_pulse_width=0.0025)
 
 class Servo():
-    def ActivateServo(self, status):
+
+    def ActivateServo(self, status, speed):
+        
+        #future implementation of speed for iris shutter.
+        #need to use for loops and iterate through angles on the servo with delay calculated.
+        #speed should be input as 1-10
+        speed = speed / 1000
+
         if status == "close":
             servo.angle = 250
         if status == "open":
             servo.angle = 80
     def run_demonstration(self):
-        self.ActivateServo("open")
+        self.ActivateServo("open",10)
         sleep(5)
-        self.ActivateServo("close")
+        self.ActivateServo("close",10)
         sleep(5)
 
+
+#Testing Function, only run if script opened inside of ServoControl.py
 if __name__ == "__main__":
-#TESTING BELOW THIS LINE COMMENT OUT TO USE IN OTHER FILES
     command = Servo()
-    command.ActivateServo("open")
+    command.ActivateServo("open", 10)
     sleep(5)
-    command.ActivateServo("close")
+    command.ActivateServo("close",10)
     sleep(5)
