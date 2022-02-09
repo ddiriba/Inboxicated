83b817a83b7c95eb077e9fdb43445fb1c91184ee
JohnBaumb <80135794+JohnBaumb@users.noreply.github.com>
2022-02-08 22:19:39 -0800

    Update StepperControl.py

--- MotorControl/StepperControl.py
+++ MotorControl/StepperControl.py
@@ -11,43 +11,43 @@ from time import sleep
 
 class Stepper():
 
-__CLOCKWISE = 1
-__ANTI_CLOCKWISE = 0
+    __CLOCKWISE = 1
+    __ANTI_CLOCKWISE = 0
 
-def __init__(self, stepPin, directionPin, delay=0.208, stepsPerRevolution=200):
+    def __init__(self, stepPin, directionPin, delay=0.208, stepsPerRevolution=200):
 
-# Configure instance
-self.CLOCKWISE = self.__CLOCKWISE
-self.ANTI_CLOCKWISE = self.__ANTI_CLOCKWISE
-self.StepPin = stepPin
-self.DirectionPin = directionPin
-self.Delay = delay
-self.RevolutionSteps = stepsPerRevolution
-self.CurrentDirection = self.CLOCKWISE
-self.CurrentStep = 0
+        # Configure instance
+        self.CLOCKWISE = self.__CLOCKWISE
+        self.ANTI_CLOCKWISE = self.__ANTI_CLOCKWISE
+        self.StepPin = stepPin
+        self.DirectionPin = directionPin
+        self.Delay = delay
+        self.RevolutionSteps = stepsPerRevolution
+        self.CurrentDirection = self.CLOCKWISE
+        self.CurrentStep = 0
 
-# Setup gpio pins
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(self.StepPin, GPIO.OUT)
-GPIO.setup(self.DirectionPin, GPIO.OUT)
+        # Setup gpio pins
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.StepPin, GPIO.OUT)
+        GPIO.setup(self.DirectionPin, GPIO.OUT)
 
-def Step(self, stepsToTake, direction = __CLOCKWISE):
+    def Step(self, stepsToTake, direction = __CLOCKWISE):
 
-print("Step Pin: " + str(self.StepPin) + " Direction Pin: " + str(self.DirectionPin) + " Delay: " + str(self.Delay))
-print("Taking " + str(stepsToTake) + " steps.")
+        print("Step Pin: " + str(self.StepPin) + " Direction Pin: " + str(self.DirectionPin) + " Delay: " + str(self.Delay))
+        print("Taking " + str(stepsToTake) + " steps.")
 
-# Set the direction
-GPIO.output(self.DirectionPin, direction)
+        # Set the direction
+        GPIO.output(self.DirectionPin, direction)
 
-# Take requested number of steps
-for x in range(stepsToTake):
-print("Step " + str(x))
-GPIO.output(self.StepPin, GPIO.HIGH)
-self.CurrentStep += 1
-sleep(self.Delay)
-GPIO.output(self.StepPin, GPIO.LOW)
-sleep(self.Delay)
+        # Take requested number of steps
+        for x in range(stepsToTake):
+            print("Step " + str(x))
+            GPIO.output(self.StepPin, GPIO.HIGH)
+            self.CurrentStep += 1
+            sleep(self.Delay)
+            GPIO.output(self.StepPin, GPIO.LOW)
+            sleep(self.Delay)
 
 # Define pins
 STEP_PIN = 16
