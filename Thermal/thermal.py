2c3cbd84b61f6b6635e023c1a98d69eb9e2258c1
JohnBaumb <johnbaumb@nevada.unr.edu>
2022-02-14 19:33:46 -0800

    couple changes

--- Thermal/thermal.py
+++ Thermal/thermal.py
@@ -213,9 +213,10 @@ if __name__ == '__main__':
     print("fps:",1/(t-t0))
     t0 = time()
     r = cam.read()
-    print(type(r))
     cv2.imshow("Seek", cam.rescale(r))
     if cv2.waitKey(1) & 0xFF == ord('q'):
       cv2.imwrite("john.png", cam.rescale(r))
+      cv2.destroyAllWindows()
+      break
       
     cv2.waitKey(1)
