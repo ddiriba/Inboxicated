de68a8081c8af258319e8dddb3f1488f05621b3e
JohnBaumb <80135794+JohnBaumb@users.noreply.github.com>
2022-02-15 00:19:02 -0800

    Update spitest.py

--- Encoder/spitest.py
+++ Encoder/spitest.py
@@ -1,40 +1,26 @@
 import smbus
 
-bus = smbus.SMBus(1)
-DEVICE_ADDRESS = 0x36      #7 bit address (will be left shifted to add the read>
-DEVICE_REG_MODE1 = 0x00
-DEVICE_REG_LEDOUT0 = 0x1d
+bus = smbus.SMBus(1)  #i2c bus number of gpio pins is 1
+DEVICE_ADDRESS = 0x36  #address of i2c device, AS5600 (sudo i2cdetect -y 1) (1 is the i2c bus number)
 
 def ReadRawAngle():
 
-    #7:0 - bits
-    lowbyte = bus.read_byte_data(DEVICE_ADDRESS, 0x0D)
+    # 7:0 - 4 bits
+    lowbyte = bus.read_byte_data(DEVICE_ADDRESS, 0x0D) #address from AS5600 Datasheet -> https://ams.com/en/as5600#tab/documents
         
-    #11:8 - 4 bits
+    # 11:8 - 4 bits
     highbyte = bus.read_byte_data(DEVICE_ADDRESS,0x0C)
     
-    #4 bits have to be shifted to its proper place as we want to build a 12-bit number
-    highbyte = highbyte << 8 #shifting to left
-    #What is happening here is the following: The variable is being shifted by 8 bits to the left:
-    #Initial value: 00000000|00001111 (word = 16 bits or 2 bytes)
-    #Left shifting by eight bits: 00001111|00000000 so, the high byte is filled in
+    # bits have to be shifted to its proper place as we want to build a 12-bit number
+    highbyte = highbyte << 8 # bitshift to left by 8 digits
+    The variable is being shifted by 8 bits to the left:
+    rawAngle = highbyte | lowbyte; 
     
-    #Finally, we combine (bitwise OR) the two numbers:
-    #High: 00001111|00000000
-    #Low:  00000000|00001111
-    #      -----------------
-    #H|L:  00001111|00001111
-    rawAngle = highbyte | lowbyte; #int is 16 bits (as well as the word)
-
-    #We need to calculate the angle:
-    #12 bit -> 4096 different levels: 360° is divided into 4096 equal parts:
-    #360/4096 = 0.087890625
-    #Multiply the output of the encoder with 0.087890625
-    degAngle = rawAngle * 0.087890625; 
+    # 12 bit -> 4096 different levels: 360 is divided into 4096 equal parts:
+    # 360/4096 = 0.087890625
+    # Multiply the output by 0.087890625
     
-    #Serial.print("Deg angle: ");
-    #Serial.println(degAngle, 2); #absolute position of the encoder within the 0-360 circle
-    
+    degAngle = rawAngle * 0.087890625;     
     print(degAngle)
 
 def main():
