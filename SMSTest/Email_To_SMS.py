293153674557bac0ad70f3764d23e87cab1ccc41
Dawit Diriba <48427979+ddiriba@users.noreply.github.com>
2022-03-09 20:48:58 -0800

    working send sms messages

--- SMSTest/Email_To_SMS.py
+++ SMSTest/Email_To_SMS.py
@@ -1,34 +1,77 @@
 import email, smtplib, ssl
 from providers import PROVIDERS
+import requests
+import urllib3
+import time
+import json
 
+def getCarrier(number):
+    carrier = ''
+    returned_status = 429
+    returned_text = ''
+    
+    #too many requests status code
+    while returned_status == 429: 
+        url = 'https://api.telnyx.com/v1/phone_number/1' + number
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        request_returned = requests.get(url, verify = False)
+        returned_text = request_returned.text
+        returned_status = request_returned.status_code
+        time.sleep(2) #wait 2 seconds
+    
+    try:
+        data = json.loads(returned_text)
+        carrier = str(data["carrier"]["name"])
+        return carrier
+    except:
+        return 'NotFound'
 
-#number@gateway-domain.com
+def send_sms_via_email(number, message, provider):
 
-def send_sms_via_email(
-    number: str,
-    message: str,
-    provider: str,
-    sender_credentials: tuple,
-    subject: str = "sent using etext",
-    smtp_server: str = "smtp.gmail.com",
-    smtp_port: int = 465,
-                ):
-    sender_email, email_password = sender_credentials
+    sender_email = "inboxicatedteam@gmail.com"
+    email_password = "tvytfdhryqkafpys" #very very safe
+    smtp_server = "smtp.gmail.com"
+    subject = "Inboxicated Notifcation"
+    smtp_port= 465
     receiver_email = f'{number}@{PROVIDERS.get(provider).get("sms")}'
+    print(receiver_email)
 
     email_message = f"Subject:{subject}\nTo:{receiver_email}\n{message}"
 
-    with smtplib.SMTP_SSL(
-        smtp_server, smtp_port, context=ssl.create_default_context()
-    ) as email:
+    with smtplib.SMTP_SSL(smtp_server, 
+                          smtp_port, 
+                          context=ssl.create_default_context()
+                          ) as email:
         email.login(sender_email, email_password)
         email.sendmail(sender_email, receiver_email, email_message)
 
-def main():
-    number = "5623720883"
-    message = "hello world!"
-    provider = "Verizon"
+def send_too_many_attempts_alert(number, provider):
 
-    sender_credentials = ("email@domain.com", "password")
+    message = "A user has tried to open the box way too many times, please assist user at the box!"
+    
+    send_sms_via_email(number, message, provider)
 
-    send_sms_via_email(number, message, provider, sender_credentials)
+def send_override_request(number, provider):
+    #number@gateway-domain.com
+    
+    message = "A user requires your assistance, please go to the box to assist!"
+    
+    send_sms_via_email(number, message, provider)
+
+def testing_main(keeper_number):
+    
+    network = getCarrier(keeper_number)
+    keeper_provider = ''
+    if "T-MOBILE" in network:
+        keeper_provider = "T-Mobile"
+    elif "VERIZON" in network:
+        keeper_provider = "Verizon"
+    elif "NEW CINGULAR" in network:
+        keeper_provider = "AT&T"
+    if keeper_provider == '':
+        print('unable to find provider of phone')
+    else:
+        send_too_many_attempts_alert(keeper_number, keeper_provider)
+        send_override_request(keeper_number, keeper_provider)
+
+testing_main("7753134694")
--- SMSTest/Get_Phone_Carrier_v2.py
+++ SMSTest/Get_Phone_Carrier_v2.py
@@ -17,30 +17,28 @@ def getCarrier(number):
         returned_status = request_returned.status_code
         time.sleep(2) #wait 2 seconds
 
-    print(returned_text)
-    print('\n')
-    print(returned_status)
-    print('\n')
-
     try:
         data = json.loads(returned_text)
         carrier = str(data["carrier"]["name"])
         return carrier
     except:
-        return 'fucky shit'
+        return 'NotFound'
     
+if __name__ == '__main__':
+    #clean_up()#run after
 
-phone_number_list = ['7754008918', '7753575103', '7025170712', 
-                     '7025334544', '7756227397', '7753387648', 
-                     '7753134694', '7023321538']
+    phone_number_list = ['7754008918', '7753575103', '7025170712', 
+                         '7025334544', '7756227397', '7753387648', 
+                         '7753134694', '7023321538', '2068181776']
 
-name_list = ['Dawit', 'Carli', 'Dani', 
-             'Jake', 'Andrew', 'Julia' , 
-             'John', 'Jervyn']
 
-carrier_list = []
-for i in phone_number_list:
-    carrier_list.append(getCarrier(i))
+    name_list = ['Dawit', 'Carli', 'Dani', 
+                 'Jake', 'Andrew', 'Julia' , 
+                 'John', 'Jervyn', 'Julia Coach']
 
-for j in range(len(phone_number_list)):
-    print(name_list[j] + ' : ' + phone_number_list[j] + ' : ' + carrier_list[j])
+    carrier_list = []
+    for i in phone_number_list:
+        carrier_list.append(getCarrier(i))
+
+    for j in range(len(phone_number_list)):
+        print(name_list[j] + ' : ' + phone_number_list[j] + ' : ' + carrier_list[j])
