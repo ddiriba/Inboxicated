7b9d7187feb1bdc65fd9384190edbfb542e12ca7
JohnBaumb <80135794+JohnBaumb@users.noreply.github.com>
2022-04-07 01:03:13 -0700

    PiGPIO fallback

---
+++ MotorControl/ServoControl3.py
@@ -0,0 +1,67 @@
+from gpiozero import Servo
+from time import sleep
+
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+factory = PiGPIOFactory()
+servo = Servo(24, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
+
+
+
+
+class Servo():
+    def __init__(self):
+        print("Servo Constructor Called.")
+    def ActivateServo(self, status, speed):
+        #speed should be an integer value, 0 results in highest possible speed of servo.
+        #HIGHER NUMBERS RESULT IN LOWER SPEEDS DUE TO SLEEP CALCULATION IN FOR LOOP.
+        
+        try:
+            speed = speed / 1000
+        except:
+            speed = 0
+            pass
+
+        if status == "close":
+            #for i in range(80,250,1):
+            servo.value = -0.3
+            #sleep(speed)
+            #servo.angle = 250
+        if status == "open":
+            #for i in range(250,80,-1):
+            servo.value = 0.55
+            #sleep(speed)
+            #servo.angle = 80
+    def run_demonstration(self):
+        self.ActivateServo("open",10)
+        sleep(5)
+        self.ActivateServo("close",10)
+        sleep(5)
+        
+    #def __del__(self):
+        #self.servo.close()
+
+#Testing Function, only run if script opened inside of ServoControl.py
+if __name__ == "__main__":
+    command = Servo()
+    command.ActivateServo("open", 10)
+    sleep(2)
+    #command.ActivateServo("close",10)
+    #sleep(2)
+    servo.value = -0.95
+    sleep(2)
+    servo.value = None
+    
+    '''print("Start in the middle")
+    servo.mid()
+    sleep(5)
+    print("Go to min")
+    servo.min()
+    sleep(5)
+    print("Go to max")
+    servo.max()
+    sleep(5)
+    print("And back to middle")
+    servo.mid()
+    sleep(5)
+    servo.value = None'''
